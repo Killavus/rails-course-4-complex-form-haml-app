733468b8ccb5ee03831ec66b21e716caf5b550b6
Marcin Grzywaczewski <killavus@gmail.com>
2014-11-02 03:35:32 +0100

    Changed schema of database after adding migration.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141102023036) do
+ActiveRecord::Schema.define(version: 20141102023154) do
 
   create_table "genres", force: true do |t|
     t.string   "name"
@@ -33,6 +33,8 @@ ActiveRecord::Schema.define(version: 20141102023036) do
     t.string   "title"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "artist"
+    t.string   "album"
   end
 
 end
