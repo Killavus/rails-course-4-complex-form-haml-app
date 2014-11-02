0730759e0bae223f38e247a308d213d40fb9c500
Marcin Grzywaczewski <killavus@gmail.com>
2014-11-02 03:53:37 +0100

    Fixed problem with an association - wrong dependent option provided

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,7 +2,7 @@ class Genre < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   validates :rating, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 10 }
 
-  has_many :songs, dependent: :destroy_all
+  has_many :songs, dependent: :delete_all
 
   def to_s
     self.name
